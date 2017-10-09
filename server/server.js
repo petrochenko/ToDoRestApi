00d0060d9db9de43c61bf38424dbc418194fa7bb
Sergey Petrochenko <mail.petrochenko@gmail.com>
2017-10-09 23:24:55 +0300

    Add PATCH /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
-let express = require('express');
-let bodyParser = require('body-parser');
-let {ObjectID} = require('mongodb');
+const _ = require('lodash');
+const express = require('express');
+const bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 let {mongoose} = require('./db/mongoose');
 let {Todo} = require('./models/todo');
@@ -43,8 +44,8 @@ app.get('/todos/:id', (req, res) => {
       }
       res.send({todo});
     }).catch((error) => {
-      res.status(400).send(error);
-    });
+    res.status(400).send(error);
+  });
 });
 
 app.delete('/todos/:id', (req, res) => {
@@ -62,6 +63,35 @@ app.delete('/todos/:id', (req, res) => {
     }).catch((error) => {
     res.status(400).send(error);
   });
+});
+
+app.patch('/todos/:id', (req, res) => {
+  let id = req.params.id;
+  let body = _.pick(req.body, ['text', 'completed']);
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  if (_.isBoolean(body.completed) && body.completed) {
+    body.completedAt = new Date().getTime();
+  } else {
+    body.completed = false;
+    body.completedAt = null;
+  }
+
+  console.log(body);
+
+  Todo
+    .findByIdAndUpdate(id, {$set: body}, {new: true})
+    .then((todo) => {
+      if (!todo) {
+        return res.status(404).send();
+      }
+      res.send({todo});
+    }).catch((error) => {
+    res.status(400).send(error);
+  });
+
 });
 
 if (!module.parent) {
