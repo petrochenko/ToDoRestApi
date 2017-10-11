7cf6468f86cb9bf84e11f5021df4a6970e44c1a0
Sergey Petrochenko <mail.petrochenko@gmail.com>
2017-10-11 23:38:09 +0300

    Add POST /users/login route

--- server/server.js
+++ server/server.js
@@ -110,10 +110,23 @@ app.get('/users/me', authenticate, (req, res) => {
   res.send(req.user);
 });
 
+app.post('/users/login', (req, res) => {
+  let body = _.pick(req.body, ['email', 'password']);
+  User.findByCredentials(body.email, body.password).then((user) => {
+    return user.generateAuthToken().then((token) => {
+      res.header('x-auth', token).send(user);
+    });
+  }).catch((err) => {
+    res.status(400).send(err);
+  });
+});
+
 if (!module.parent) {
   app.listen(port, () => {
     console.log(`Application is started on port + ${port}`);
   });
 }
+
+
 
 module.exports = {app};
