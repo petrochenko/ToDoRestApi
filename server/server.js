4edf85b0c90596cf08cb325605e0b38ef3bbd902
Sergey Petrochenko <mail.petrochenko@gmail.com>
2017-10-12 09:33:28 +0300

    Add DELETE users/me/token route

--- server/server.js
+++ server/server.js
@@ -121,6 +121,14 @@ app.post('/users/login', (req, res) => {
   });
 });
 
+app.delete('/users/me/token', authenticate, (req, res) => {
+  req.user.removeToken(req.token).then(() => {
+    res.status(200).send();
+  }, () => {
+    res.status(400).send();
+  });
+});
+
 if (!module.parent) {
   app.listen(port, () => {
     console.log(`Application is started on port + ${port}`);
